7d9b9f1d412f2020724543befbd2e215d77ab8a1
Julien <julien.vesy@gmail.com>
2014-12-15 21:46:33 +0800

    adding fragment, parsing data and loading

--- app/src/main/java/team_10/nourriture_android/MainActivity.java
+++ app/src/main/java/team_10/nourriture_android/MainActivity.java
@@ -1,39 +1,25 @@
 package team_10.nourriture_android;
 
-import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            getAllRecipes();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
         Button firstButton = (Button) findViewById(R.id.firstButton);
         firstButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.contentFrame, new DishesFragment()).commit();
+                fragmentManager.beginTransaction().replace(R.id.contentFrame, new RecipesFragment()).commit();
             }
         });
     }
@@ -61,22 +47,5 @@ public class MainActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void getAllRecipes() throws JSONException {
-        NourritureRestClient.get("recipes", null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                // If the response is JSONObject instead of expected JSONArray
-            }
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
-                // Pull out the first event on the public timeline
-               // JSONObject firstEvent = timeline.get(0);
-                //String tweetText = firstEvent.getString("text");
-
-                // Do something with the response
-                Log.e("app", timeline.toString());
-            }
-        });
-    }
 }
---
+++ app/src/main/java/team_10/nourriture_android/RecipesFragment.java
@@ -0,0 +1,78 @@
+package team_10.nourriture_android;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class RecipesFragment extends Fragment {
+
+    ArrayList<String> recipesNameList;
+    ListView recipesList;
+    ProgressDialog progress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipes, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recipesList = (ListView) getActivity().findViewById(R.id.listViewRecipes);
+        progress = new ProgressDialog(getActivity());
+        progress.setTitle("Processing");
+        progress.setMessage("Loading...");
+        progress.show();
+        try {
+            getAllRecipes();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getAllRecipes() throws JSONException {
+        NourritureRestClient.get("recipes", null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                progress.dismiss();
+                try {
+                    recipesNameList = new ArrayList<>();
+                    JSONObject item;
+                    for (int i = 0; i != response.length(); ++i) {
+                        item = response.getJSONObject(i);
+                        recipesNameList.add(item.getString("name"));
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, recipesNameList);
+                    recipesList.setAdapter(arrayAdapter);
+                } catch (JSONException | NullPointerException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
